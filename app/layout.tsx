5619badae4a4820c8b3f6e36d3d91fb491e853ba
HHuzaifa <90773074+HHuzaifa@users.noreply.github.com>
2023-03-23 16:43:21 +0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import Header from "@/app/components/Header";
+import Header from "@/components/Header";
 import Head from "./head";
-import Footer from "@/app/components/Footer";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
